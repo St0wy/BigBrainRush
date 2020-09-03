caf8fa495d4af1b020635fc590cf3faa51392cb0
ACKERMANNGUE <gawen.ackrm@eduge.ch>
2020-09-03 16:08:47 +0200

    Creation grille + outline on mouseover
    Création d'une grille de 25x25
    ainsi que de l'ajout d'une outligne lors du mouseover (encore en dév)

---
+++ Assets/Scripts/GenerateGrid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateGrid : MonoBehaviour
+{
+    private const int GRID_SIZE = 25;
+
+    private Color baseColor;
+
+    public List<GameObject> roadsPrefab;
+    public GameObject[,] roads = new GameObject[GRID_SIZE, GRID_SIZE];
+    public Camera mainCamera;
+    public Transform startPos;
+
+    void Start()
+    {
+        if (roadsPrefab == null)
+        {
+            roadsPrefab = new List<GameObject>();
+        }
+        InitGrid(roadsPrefab, GRID_SIZE, mainCamera);
+    }
+    /// <summary>
+    /// Initialise la grille
+    /// </summary>
+    /// <param name="pRoads">La liste des prefabs de routes</param>
+    /// <param name="sizeGrid">la taille de la grille</param>
+    /// <param name="cam">la caméra</param>
+    private void InitGrid(List<GameObject> pRoads, int sizeGrid, Camera cam)
+    {
+        GameObject go = pRoads[2];
+        int sizeBlock = cam.scaledPixelWidth / sizeGrid;
+        go.transform.localScale = new Vector3(sizeBlock, sizeBlock, sizeBlock);
+        for (int x = 0; x < sizeGrid; x++)
+        {
+            for (int y = 0; y < sizeGrid; y++)
+            {
+                GameObject instantiatedPrefab = Instantiate(go, new Vector3(startPos.transform.position.x + x * sizeBlock, startPos.transform.position.y, startPos.transform.position.z - y * sizeBlock), Quaternion.identity);
+                instantiatedPrefab.AddComponent<OutlineBox>().go = instantiatedPrefab;
+            }
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/OutlineBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineBox : MonoBehaviour
+{
+    public GameObject go;
+
+    void OnMouseEnter()
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        go.GetComponent<Renderer>().material.SetShaderPassEnabled("Custom/chr_outline", true);
+    }
+    void OnMouseExit()
+    {
+        go.GetComponent<Renderer>().material.SetShaderPassEnabled("Custom/chr_outline", false);
+    }
+}
